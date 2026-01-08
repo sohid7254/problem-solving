c6f41c301b6583853bdfeb9880b045df986a20d3
sohid7254 <sohidameen321@gmail.com>
2026-01-08 17:55:02 +0600

    added problem-8

--- problems.js
+++ problems.js
@@ -82,3 +82,13 @@ function findEven(arr) {
   return evens;
 }
 console.log(findEven([1, 2, 3, 4, 5, 6]));
+
+// Problem 8: Capitalize First Letter of Each Word
+function capitalizeWords(str) {
+  let words = str.split(" ");
+  for (let i = 0; i < words.length; i++) {
+    words[i] = words[i][0].toUpperCase() + words[i].slice(1);
+  }
+  return words.join(" ");
+}
+console.log(capitalizeWords("hello world"));
